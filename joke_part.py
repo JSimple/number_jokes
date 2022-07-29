96786b687fbedc966e4d47ea4ed42b6778a48f64
JSimple <josephinemayasimple@gmail.com>
2022-07-28 21:51:54 -0400

    add: json encoding for joke parts, refactored add_points method

--- joke_part.py
+++ joke_part.py
@@ -1,5 +1,15 @@
 from numpy.polynomial import Polynomial as P
 from random import *
+import json
+
+class PolynomialEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, P):
+            return({
+                'coef' : list(obj.coef),
+                'str' : str(obj)
+                })
+        return super().default(obj)
 
 class PolynomialJokePart:
     def __init__(self, points: list = [], prev_points: list = [], polynomial = None, rounding: int = 2) -> None:
@@ -9,6 +19,9 @@ class PolynomialJokePart:
         self.polynomial = polynomial
         self.type = 'polynomial'
     
+    def json(self):
+        return json.dumps(self.__dict__, cls=PolynomialEncoder)
+    
     def clear_attributes(self):
         '''
         Sets all of the JokePart's attributes to empty.
@@ -17,27 +30,26 @@ class PolynomialJokePart:
         self.points = []
         self.all_points = []
         self.polynomial = P()
-        
-    def add_points(self, num_points: int = 0, points: list = [], rounding = 5):
-        '''
-        Append numbers to the JokePart's points attribute.\
-        If a list of points is specified, the function will append that list.\
-        Otherwise the function will append a specified number of points that follow the JokePart's polynomial attribute. 
-        '''
-        if points:
-            num_points = len(points)
-            float_points = [float(p)for p in points]
-            self.points += float_points
-            self.all_points += float_points
-        elif num_points:
-            all_points_len = len(self.all_points)
-            for i in range(all_points_len, all_points_len + num_points):
-                pt = round(self.polynomial(i), rounding)
-                self.points.append(pt)
-                self.all_points.append(pt)
-        else:
-            pass
     
+    def add_points(self, num_points: int = 0, rounding = 5):
+        '''Generate and append to JokePart's points attribute a specified number of points that follow the JokePart's polynomial attribute
+        '''
+        all_points_len = len(self.all_points)
+        for i in range(all_points_len, all_points_len + num_points):
+            pt = round(self.polynomial(i), rounding)
+            self.points.append(pt)
+            self.all_points.append(pt)
+    
+    def add_custom_points(self, points: list = []):
+        """Append a list of numbers to the JokePart's points attribute.
+
+        Args:
+            points (list, optional): the points to be added. Defaults to [].
+        """
+        float_points = [float(p)for p in points]
+        self.points += float_points
+        self.all_points += float_points
+
     def fit_polynomial(self):
         '''
         Fits a polynomial function to the JokePart's points attribute.\
@@ -51,3 +63,9 @@ class PolynomialJokePart:
         self.polynomial = p
 
 
+pjp = PolynomialJokePart()
+print(pjp.json())
+pjp.add_custom_points([4,2])
+print('now with points:\n', pjp.json())
+pjp.fit_polynomial()
+print('now with a polynomial:\n', pjp.json())
--- polynomial_gardenpath.py
+++ polynomial_gardenpath.py
@@ -1,7 +1,6 @@
-from sympy import degree
 from joke_part import PolynomialJokePart as JP
 from numpy.polynomial import Polynomial as P
-from random import random, choice
+from random import random
 
 class PolynomialGardenpath():
     
@@ -12,13 +11,12 @@ class PolynomialGardenpath():
         self.joke_parts = []
     
     def add_joke_part_w_points(self, starting_points: list, auto_fill: int = 0):
-        '''
-        Creates a new section (JokePart) of the joke, using a list of numbers, and appends it to the PolynomialGardenpath's joke_parts attribute.
+        """Creates a new section (JokePart) of the joke, using a list of numbers, and appends it to the PolynomialGardenpath's joke_parts attribute.
         
-        Parameters:
+        Args:
             starting_points : a list of numbers that will be used to determine the JokePart's polynomial
             auto_fill : an integer representing how many more points to fill in after starting_points. These points are filled in using the JokePart's polynomial.
-        '''
+        """
         # gather all the points from all the previous jokeparts
         print ('jokeparts: ', self.joke_parts)
         if not self.joke_parts:
@@ -40,13 +38,12 @@ class PolynomialGardenpath():
         self.joke_parts.append(jp)
         
     def add_joke_part_w_polynomial(self, polynomial, num_pts):
-        '''
-        Creates a new section (JokePart) of the joke, using a numpy Polynomial and a number of points, and appends the JokePart to the PolynomialGardenpath's joke_parts attribute.
+        """Creates a new section (JokePart) of the joke, using a numpy Polynomial and a number of points, and appends the JokePart to the PolynomialGardenpath's joke_parts attribute.
         
-        Parameters:
+        Args:
             polynomial : a numpy Polynomial used to determine the points in the JokePart
             num_pts : the number of points in the desired joke part
-        '''
+        """
         # parameter error handling
         terms = len(polynomial.coef)
         if num_pts < terms:
@@ -62,66 +59,107 @@ class PolynomialGardenpath():
         self.joke_parts.append(jp.points)
     
     def _gen_pts(self, len: int = 2, val_range: tuple = (-1.0,10.0), rounding: int = 0):
+        """Utility function that generates a list of random points.
+        
+        Args:
+            len (int, optional): length of list. Defaults to 2.
+            val_range (tuple, optional): the range for the values in the list. Defaults to (-1.0,10.0).
+            rounding (int, optional): how many significant digits below the decimal point each number has. Defaults to 0.
+
+        Returns:
+            None
+        """
         mini, maxi = val_range
         return [round(mini + (maxi - mini) * random(), rounding) for i in range(len)]
     
     def _gen_polynomial(self, degree: int = 1, coef_range: tuple = (-10.0,10.0)):
+        """Utility function that generates a random polynomial that can be used to create JokeParts.
+
+        Args:
+            degree (int, optional): the polynomial's highest degree. Defaults to 1.
+            coef_range (tuple, optional): the value range for the polynomial's coeficients. Defaults to (-10.0,10.0).
+
+        Returns:
+            _type_: _description_
+        """
         terms = degree + 1
         coefs = self._gen_pts(terms,coef_range)
         p = P(coefs)
         return p
     
     def _validate_gen_params(self, jp_degree_and_length):
-        """
-        Makes sure that valid parameters are being passed to random joke generation methods.
+        """Makes sure that valid parameters are being passed to random joke generation methods.
+
+        Args:
+            jp_degree_and_length (list of tuples): _description_
         """
         prev_total_length = 0
+        if self.joke_parts:
+            prev_total_length = len(self.joke_parts[-1].all_points)
+        
         for degree, length in jp_degree_and_length:
             assert (degree >= 0 and length > 0),"A JokePart must have a positive length and a polynomial with a positive degree."
             assert (prev_total_length <= degree),"A JokePart must have a polynomial with an equal or higher degree to the total number of points in the joke leading up to it."
             assert (degree < prev_total_length + length),"A JokePart's polynomial degree must be smaller than the number of points in the joke up to and including the JokePart."
             prev_total_length += length
     
-    def gen_classic_joke(self, jp_degree_and_length = [(1,4),(4,3)], range_scaling = 1, rounding = 1):
+    def _get_all_points(self):
+        all_points = []
+        if self.joke_parts:
+            all_points = self.joke_parts[-1].all_points
+        
+        return all_points
+        
+    
+    def add_gen_joke_parts(self, jp_degree_and_length: list, range_scaling = 1, rounding = 0):
         """_summary_
 
         Args:
-            jp_terms_and_length (list, optional): _description_. Defaults to [(2,4),(5,3)].
+            jp_degree_and_length (list, optional): _description_. Defaults to [(2,4),(5,3)].
         """
-        #clear jokeparts list
-        self.joke_parts = []
         # Validate parameters
         self._validate_gen_params(jp_degree_and_length)
         
         # generate each joke part
-        total_prev_points = []
+        all_prev_points = self._get_all_points()
         biggest_point_so_far = 0
+        if all_prev_points:
+            biggest_point_so_far = max(all_prev_points,key=abs)
         random_points_range = (-5,10)
         for i,(degree,length) in enumerate(jp_degree_and_length):
             if biggest_point_so_far:
                 random_point_max = biggest_point_so_far ** range_scaling
                 random_points_range = (-1 * random_point_max , random_point_max) 
-            random_points = self._gen_pts(degree + 1 - len(total_prev_points), random_points_range, rounding)
-            jp = JP(random_points, total_prev_points)
+            random_points = self._gen_pts(degree + 1 - len(all_prev_points), random_points_range, rounding)
+            jp = JP(random_points, all_prev_points)
             jp.fit_polynomial()
             jp.add_points(length - len(random_points))
-            total_prev_points += jp.points
+            all_prev_points += jp.points
             biggest_point_in_jp = max(jp.points,key=abs)
             biggest_point_so_far = max([biggest_point_so_far,biggest_point_in_jp],key=abs)
             self.joke_parts.append(jp)
     
-    # gen poly jp params
-    # gen point jp params
-        
+    def gen_joke(self, jp_degree_and_length = [(1,4),(4,3)], range_scaling = 3, rounding = 0):
+        """Generates a new polynomial gardenpath number joke, and sets it as as self.joke_parts.
+
+        Args:
+            jp_degree_and_length (list, optional): A list of tuples representing parameters (length, polynomial degree) for each of the joke's JokeParts. Defaults to [(1,4),(4,3)].
+            range_scaling (int, optional): _description_. Defaults to 3.
+            rounding (int, optional): _description_. Defaults to 1.
+        """
+        self.clear()
+        self.add_gen_joke_parts(jp_degree_and_length, range_scaling, rounding)
     
-    #TODO: define generate setup, add as many punchlines as you want
         
 pgp = PolynomialGardenpath()
 
-pgp.gen_classic_joke([(0,1),(1,1),(2,1),(3,1),(4,1),(5,1)], 2, 6)
+pgp.gen_joke([(0,1),(1,1)])
 for jp in pgp.joke_parts:
     print(jp.points,jp.polynomial)
-    
+pgp.add_gen_joke_parts([(2,1),(3,1)])
+for jp in pgp.joke_parts:
+    print(jp.points,jp.polynomial)    
+print(pgp._get_all_points())
     
 # Joke Param Structure
         # setup:
